19197a0960281372f98e24ad4513a01cf5462b60
Karolina <karolinaa1999@gmail.com>
2020-07-23 14:36:17 +0100

    Add Stage 4/5 project

--- Tic Tac Toe/tic_tac_toe.py
+++ Tic Tac Toe/tic_tac_toe.py
@@ -1,24 +1,59 @@
 def tic_tac_toe():
     field = list(input('Enter cells: '))
+
+    replace_empty(field)
     print_field(field)
-    game_state(field)
+
+    player_move(field)
+    print_field(field)
+    # game_state(field)
 
 
 # prints the field from the given input
 def print_field(field):
-    print('---------')
+    print('-' * 9)
     for i in range(0, len(field), 3):
         print('|', field[i], field[i + 1], field[i + 2], '|')
-    print('---------')
+    print('-' * 9)
+
+
+# replaces underscores with spaces in empty cells
+def replace_empty(field):
+    for i in range(len(field)):
+        if field[i] == '_':
+            field[i] = ' '
+
+
+def player_move(field):
+    while True:
+        try:
+            x, y = input('Enter the coordinates: ').split()
+            x = int(x)
+            y = int(y)
+        except ValueError:
+            print('You should enter numbers!')
+            continue
+
+        if x < 1 or x > 3 or y < 1 or y > 3:
+            print('Coordinates should be from 1 to 3!')
+        else:
+            # converts coordinates to index in the field list
+            index = (x - 1) + (9 - (3 * y))
+
+            if field[index] != ' ':
+                print('This cell is occupied! Choose another one!')
+            else:
+                field[index] = 'X'
+                break
 
 
 # prints the current state of the game
 def game_state(field):
     if impossible(field):
         print('Impossible')
-    elif x_wins(field):
+    elif wins(field, 'X'):
         print('X wins')
-    elif o_wins(field):
+    elif wins(field, 'O'):
         print('O wins')
     elif unfinished(field):
         print('Game not finished')
@@ -32,19 +67,9 @@ def impossible(field):
     if abs(field.count('X') - field.count('O')) > 1:
         return True
     # the field has three X in a row as well as three O in a row
-    elif x_wins(field) and o_wins(field):
+    elif wins(field, 'X') and wins(field, 'O'):
         return True
     return False
-
-
-# returns True if the field has three X in a row
-def x_wins(field):
-    return wins(field, 'X')
-
-
-# returns True if the field has three O in a row
-def o_wins(field):
-    return wins(field, 'O')
 
 
 # returns True if the given player wins
@@ -87,7 +112,7 @@ def diagonal_win(field, char):
 # returns True if the game state is unfinished
 def unfinished(field):
     # checks whether no one has won, and whether the field has empty cells
-    if not x_wins(field) and not o_wins(field) and field.count('_') > 0:
+    if not wins(field, 'X') and not wins(field, 'O') and field.count('_') > 0:
         return True
     return False
 
