c0a4d12214ac58199cb3982a76fd91d22bf8fb53
Karolina <karolinaa1999@gmail.com>
2020-07-23 19:50:21 +0100

    Add final stage project

--- Tic Tac Toe/tic_tac_toe.py
+++ Tic Tac Toe/tic_tac_toe.py
@@ -1,15 +1,17 @@
 def tic_tac_toe():
-    field = list(input('Enter cells: '))
-
-    replace_empty(field)
+    field = [' '] * 9
     print_field(field)
 
-    player_move(field)
-    print_field(field)
-    # game_state(field)
+    current_player = 'X'
+    while not game_over(field):
+        player_move(field, current_player)
+        print_field(field)
+        current_player = next_player(current_player)
+
+    game_state(field)
 
 
-# prints the field from the given input
+# prints the current game field
 def print_field(field):
     print('-' * 9)
     for i in range(0, len(field), 3):
@@ -17,14 +19,34 @@ def print_field(field):
     print('-' * 9)
 
 
-# replaces underscores with spaces in empty cells
-def replace_empty(field):
-    for i in range(len(field)):
-        if field[i] == '_':
-            field[i] = ' '
+# returns the player which has to make a move next
+def next_player(current_player):
+    return 'X' if current_player == 'O' else 'O'
 
 
-def player_move(field):
+# returns True if the game is finished
+def game_over(field):
+    if wins(field, 'X'):
+        return True
+    elif wins(field, 'O'):
+        return True
+    elif draw(field):
+        return True
+    return False
+
+
+# prints the current state of the game
+def game_state(field):
+    if wins(field, 'X'):
+        print('X wins')
+    elif wins(field, 'O'):
+        print('O wins')
+    else:
+        print('Draw')
+
+
+# allows the current player to make a move
+def player_move(field, player):
     while True:
         try:
             x, y = input('Enter the coordinates: ').split()
@@ -43,76 +65,55 @@ def player_move(field):
             if field[index] != ' ':
                 print('This cell is occupied! Choose another one!')
             else:
-                field[index] = 'X'
+                field[index] = player
                 break
 
 
-# prints the current state of the game
-def game_state(field):
-    if impossible(field):
-        print('Impossible')
-    elif wins(field, 'X'):
-        print('X wins')
-    elif wins(field, 'O'):
-        print('O wins')
-    elif unfinished(field):
-        print('Game not finished')
-    else:
-        print('Draw')
-
-
-# returns True if the game state is impossible
-def impossible(field):
-    # the field has a lot more X's than O's or vice versa
-    if abs(field.count('X') - field.count('O')) > 1:
-        return True
-    # the field has three X in a row as well as three O in a row
-    elif wins(field, 'X') and wins(field, 'O'):
-        return True
-    return False
-
-
 # returns True if the given player wins
-def wins(field, char):
-    if horizontal_win(field, char) or vertical_win(field, char)\
-            or diagonal_win(field, char):
+def wins(field, player):
+    if horizontal_win(field, player) or vertical_win(field, player)\
+            or diagonal_win(field, player):
         return True
     return False
 
 
 # returns True if the given player wins horizontally
-def horizontal_win(field, char):
+def horizontal_win(field, player):
     # checks all three rows
     for i in range(0, len(field), 3):
-        if field[i] == char and field[i + 1] == char and field[i + 2] == char:
+        if field[i] == player and field[i + 1] == player \
+                and field[i + 2] == player:
             return True
     return False
 
 
 # returns True if the given player wins vertically
-def vertical_win(field, char):
+def vertical_win(field, player):
     # checks all three columns
     for i in range(len(field) // 3):
-        if field[i] == char and field[i + 3] == char and field[i + 6] == char:
+        if field[i] == player and field[i + 3] == player \
+                and field[i + 6] == player:
             return True
     return False
 
 
 # returns True if the given player win diagonally
-def diagonal_win(field, char):
+def diagonal_win(field, player):
     # checks both diagonals
-    if field[4] == char:
-        if field[0] == char and field[8] == char:
+    if field[4] == player:
+        if field[0] == player and field[8] == player:
             return True
-        elif field[2] == char and field[6] == char:
+        elif field[2] == player and field[6] == player:
             return True
     return False
 
 
-# returns True if the game state is unfinished
-def unfinished(field):
-    # checks whether no one has won, and whether the field has empty cells
-    if not wins(field, 'X') and not wins(field, 'O') and field.count('_') > 0:
+# returns True if the game is finished and no one has won
+def draw(field):
+    # checks whether no one has won,
+    # and whether the field does not have empty cells
+    if not wins(field, 'X') and not wins(field, 'O') \
+            and field.count(' ') == 0:
         return True
     return False
 
